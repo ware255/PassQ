ad875b54046e42db20dea34d0a4719c26589c56c
われ <74850252+ware255@users.noreply.github.com>
2022-07-12 16:14:41 +0900

    Update main.f90

--- src/main.f90
+++ src/main.f90
@@ -12,14 +12,18 @@ program main
                                 &'w', 'x', 'y', 'z', '1', '2',&
                                 &'3', '4', '5', '6', '7', '8',&
                                 &'9', '0', '!', '"', '#', '$',&
-                                &'%', '&', "'", '(', ')', "=",&
+                                &'%', '&', '''', '(', ')', '=',&
                                 &'-', '~', '^', '|', '@', '`',&
                                 &'[', '{', ']', '}', '*', ':',&
                                 &'+', ';', '_', '?', '/', '.',&
                                 &',', '<', '>', '\']
+    character(len=1) tf
     integer(int64) seed, max, err, i, re
     seed = time()
-    print '(A, I0, A)', "1~", HUGE(seed), "までの数値を入力してください。"
+    print '(A)', "生成されたパスワードをファイルに出力しますか？[Y/N]"
+    write (*, '(A)', advance='no') ": "
+    read (*, *) tf
+    print '(A, A, I0, A)', new_line(""), "1~", HUGE(seed), "までの数値を入力してください。"
     write (*, '(A)', advance='no') ": "
     read (*, *, iostat=err) max
     if (err .ne. 0) then
@@ -30,17 +34,26 @@ program main
         stop
     end if
     print *, ""
-    do i = 1, max
-        re = mod(xor128(seed), 95)
-        write (*, '(A)', advance='no') str(re:re)
-    end do
+    if (trim(tf) .eq. "Y" .or. trim(tf) .eq. "y") then
+        open(10, file="Password.txt", status="replace")
+        do i = 1, max - 1
+            re = mod(xor128(seed), 94) + 1
+            write (10, '(A)', advance='no') str(re:re)
+        end do
+        close(10)
+    else
+        do i = 1, max - 1
+            re = mod(xor128(seed), 94) + 1
+            write (*, '(A)', advance='no') str(re:re)
+        end do
+    end if
     print '(A, A, A)', new_line(""), new_line(""), "終わるにはEnterを押してください。"
     read *
 contains
     integer(int64) function xor128(w)
         implicit none
         integer(int64), intent(inout) :: w
-        integer(int64), save :: x=123456789, y=362436069, z=521288629
+        integer(int64), save :: x = 123456789, y = 362436069, z = 521288629
         integer(int64) t
         t = xor(x, lshift(x, 11))
         x = y; y = z; z = w;
