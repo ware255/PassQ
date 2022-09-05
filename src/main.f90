14bad3dfadcd11aa08ebe7168df5ece4b346ac48
われ <74850252+ware255@users.noreply.github.com>
2022-09-05 23:34:37 +0900

    Update main.f90

--- src/main.f90
+++ src/main.f90
@@ -1,6 +1,5 @@
-program main
+module key
     use, intrinsic :: iso_fortran_env, only: int64
-    implicit none
     character(len=1) :: str(94) = ['A', 'B', 'C', 'D', 'E', 'F',&
                                 &'G', 'H', 'I', 'J', 'K', 'L',&
                                 &'N', 'M', 'O', 'P', 'Q', 'R',&
@@ -17,38 +16,7 @@ program main
                                 &'[', '{', ']', '}', '*', ':',&
                                 &'+', ';', '_', '?', '/', '.',&
                                 &',', '<', '>', '\']
-    character(len=1) tf
-    integer(int64) seed, max, err, i, re
-    seed = time()
-    print '(A)', "生成されたパスワードをファイルに出力しますか？[Y/N]"
-    write (*, '(A)', advance='no') ": "
-    read (*, *) tf
-    print '(A, A, I0, A)', new_line(""), "1~", HUGE(seed), "までの数値を入力してください。"
-    write (*, '(A)', advance='no') ": "
-    read (*, *, iostat=err) max
-    if (err .ne. 0) then
-        error stop ": オーバーフローしました."
-    else if (max <= 0) then
-        print '(A, A)', new_line(""), "* 0 <= max の条件に当てはまったので終了します."
-        read *
-        stop
-    end if
-    print *, ""
-    if (trim(tf) .eq. "Y" .or. trim(tf) .eq. "y") then
-        open(10, file="Password.txt", status="replace")
-        do i = 1, max - 1
-            re = mod(xor128(seed), 94) + 1
-            write (10, '(A)', advance='no') str(re:re)
-        end do
-        close(10)
-    else
-        do i = 1, max - 1
-            re = mod(xor128(seed), 94) + 1
-            write (*, '(A)', advance='no') str(re:re)
-        end do
-    end if
-    print '(A, A, A)', new_line(""), new_line(""), "終わるにはEnterを押してください。"
-    read *
+    integer(int64) seed, max, err, i, re, level
 contains
     integer(int64) function xor128(w)
         implicit none
@@ -60,4 +28,117 @@ contains
         w = xor(xor(w, rshift(w, 19)), xor(t, rshift(t, 8)))
         xor128 = w
     end function xor128
+end module key
+
+subroutine level_1()
+    use key
+    implicit none
+    i = 0
+    do
+        re = mod(xor128(seed), 94) + 1
+        if (re > 62 .or. re < 53) then
+            cycle
+        else if (i .eq. max) then
+            exit
+        end if
+        write (*, '(A)', advance='no') str(re:re)
+        i = i + 1
+    end do
+end subroutine level_1
+
+subroutine level_2()
+    use key
+    implicit none
+    i = 0
+    do
+        re = mod(xor128(seed), 94) + 1
+        if (re > 52 .or. re < 23) then
+            cycle
+        else if (i .eq. max) then
+            exit
+        end if
+        write (*, '(A)', advance='no') str(re:re)
+        i = i + 1
+    end do
+end subroutine level_2
+
+subroutine level_3()
+    use key
+    implicit none
+    i = 0
+    do
+        re = mod(xor128(seed), 94) + 1
+        if (re > 62 .or. re < 23) then
+            cycle
+        else if (i .eq. max) then
+            exit
+        end if
+        write (*, '(A)', advance='no') str(re:re)
+        i = i + 1
+    end do
+end subroutine level_3
+
+subroutine level_4()
+    use key
+    implicit none
+    i = 0
+    do
+        re = mod(xor128(seed), 94) + 1
+        if (re > 62) then
+            cycle
+        else if (i .eq. max) then
+            exit
+        end if
+        write (*, '(A)', advance='no') str(re:re)
+        i = i + 1
+    end do
+end subroutine level_4
+
+subroutine level_no()
+    use key
+    implicit none
+    do i = 1, max
+        re = mod(xor128(seed), 94) + 1
+        write (*, '(A)', advance='no') str(re:re)
+    end do
+end subroutine level_no
+
+program main
+    use key
+    implicit none
+    seed = time()
+    if (mod(seed, 2) .eq. 0) then
+        seed = seed * 5
+    else
+        seed = seed * 10
+    end if
+    write (*, '(A)', advance='no') "レベルを選択してください。(1~4 or 0): "
+    read (*, *) level
+    print '(A, A, I0, A)', new_line(""), "1~", HUGE(seed), "までの数値を入力してください。"
+    write (*, '(A)', advance='no') ": "
+    read (*, *, iostat=err) max
+    if (err .ne. 0) then
+        error stop ": オーバーフローしました."
+    else if (max <= 0) then
+        print '(A, A)', new_line(""), "* 0 <= max の条件に当てはまったので終了します."
+        read *
+        stop
+    end if
+    print *, ""
+    select case (level)
+    case (1)
+        call level_1()
+    case (2)
+        call level_2()
+    case (3)
+        call level_3()
+    case (4)
+        call level_4()
+    case (0)
+        call level_no()
+    case default
+        stop "エラーが発生しました。"
+    end select
+    print '(A, A, A)', new_line(""), new_line(""), "終わるにはEnterを押してください。"
+    read *
 end program main
